62a1e1b196430433218acfbcc3eb2aebb00a7fa9
pipeleon <oflg94@gmail.com>
2021-11-08 09:19:18 -0800

    First pseudo code

--- printf.c
+++ printf.c
@@ -8,25 +8,58 @@
  *
  *Return: the number of characters printed (excluding the null byte used to end output to strings)
  */
-int _printf(const char *format, ...);
+int _printf(const char *format, ...)
 {
 	va_list ap;
 	int i, j;
-	int count;
+	int cont;
 	char *buffer;
+	char *temp;
 
-	buffer = malloc(sizeof(char * 1024);
+	buffer = malloc(sizeof(char) * 1024);
 
-	format_t get_opt[]= {
+
+
+	format_t get_opt[] = {
 		{"c", set_char},
 		{"s", set-string},
 		{"i", set_decimal},
 		{"d", set_decimal},
 		{"%", print_percent},
-	};
+		{NULL, NULL}
+		};*/
 
 	if (format == NULL)
 	{
 		return (-1);
 	}
-			
+
+	i = 0;
+	cont = 0;
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			j = 0;
+			while (j < 10)
+			{
+				if (format[i + 1] == get_op->char[j])
+					break;
+				j++;
+			}
+			temp = get_op->funt[j](va_list);
+			j = 0;
+			while (temp[j] != '\0')
+			{
+				buffer[cont] = temp[j];
+				cont++;
+				j++;
+			}
+		}
+		else
+		{
+			buffer[cont] = format[i];
+			cont++;
+		}
+		i++;
+	}
