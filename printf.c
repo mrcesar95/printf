935706463cfbc79a9537943ea7d8cfbf64fda855
pipeleon <oflg94@gmail.com>
2021-11-09 11:27:36 -0800

    Adding case when theres no match after %

--- printf.c
+++ printf.c
@@ -46,13 +46,23 @@ int _printf(const char *format, ...)
 					break;
 				j++;
 			}
-			temp = (get_op[j / 2].f)(ap);
-			j = 0;
-			while (temp[j] != '\0')
+			printf("El numero de posicion es %d\n", j);
+			if (j == 10)
 			{
-				buffer[cont] = temp[j];
+				i--;
+				buffer[cont] = format[i];
 				cont++;
-				j++;
+			}
+			else
+			{
+				temp = (get_op[j / 2].f)(ap);
+				j = 0;
+				while (temp[j] != '\0')
+				{
+					buffer[cont] = temp[j];
+					cont++;
+					j++;
+				}
 			}
 		}
 		else
